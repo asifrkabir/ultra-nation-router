a5c3a0b3d4b89eda9cb898a72f15745c0d59f7d2
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2022-04-01 20:04:10 +0600

    country details render bug

--- src/components/CountryDetails/CountryDetails.js
+++ src/components/CountryDetails/CountryDetails.js
@@ -5,24 +5,24 @@ import { useParams } from 'react-router-dom';
 const CountryDetails = () => {
     const { countryId } = useParams();
 
-    const [country, setCountry] = useState({});
+    const [country, setCountry] = useState([]);
 
     useEffect(() => {
         fetch(`https://restcountries.com/v3.1/alpha/${countryId}`)
             .then(res => res.json())
-            .then(data => setCountry(data[0]))
-    }, [countryId])
+            .then(data => setCountry(data))
+    }, [])
 
-    console.log(country);
+    console.log(country[0].name.common);
 
     return (
         <Container>
             <Card className="text-center">
                 <Card.Header>
-                    <Card.Img variant="top" src={country.flags.svg} />
+                    {/* <Card.Img variant="top" src={country.flags.svg} /> */}
                 </Card.Header>
                 <Card.Body>
-                    <Card.Title>{country.name.common}</Card.Title>
+                    {/* <Card.Title>{country.name.common}</Card.Title> */}
                     <Card.Text>
                         With supporting text below as a natural lead-in to additional content.
                     </Card.Text>
